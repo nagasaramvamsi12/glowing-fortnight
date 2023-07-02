9c2d1ba1e0ca22eb49fd438ca8a1581f84f76028
nagasaramvamsi12 <103506788+nagasaramvamsi12@users.noreply.github.com>
2023-07-03 01:41:54 +0530

    first commit

---
+++ src/app/Services/courses.service.ts
@@ -0,0 +1,40 @@
+export class CoursesService{
+    courses = [
+        { id:101, name:'JavaScript developers', author: 'John Heikela', type: 'stipend', 
+          salary: 10.0, rating:4.5,image:'assets/courses/course-image-1.jpeg',
+          description: ' John Heikela is a JavaScript Developer to join our dynamic development team. As a JavaScript Developer, you will be responsible for designing, developing, and maintaining web applications using JavaScript, HTML, and CSS. You will collaborate with cross-functional teams to deliver high-quality, scalable, and performant solutions that meet our clients requirements.'
+        },
+        { id:102, name:'Angular developers', author: 'Mark Vought',  type: 'stipend', 
+          salary: 129.00,  rating:4.0, image:'assets/courses/course-image-2.jpeg',
+          description: 'Mark Vought is a talented Angular Developer to join our development team. As an Angular Developer, you will be responsible for designing, implementing, and maintaining web applications using Angular. You will collaborate with cross-functional teams to deliver high-quality, scalable, and performant solutions that meet our clients requirements.'
+        },
+        { id:103, name:'React developers', author: 'Steve Smith',  type: 'Free', 
+        salary: 0.00,  rating:3.5,image:'assets/courses/course-image-3.jpeg',
+          description: 'Steve Smith is a skilled React Developer to join our development team. As a React Developer, you will be responsible for designing, developing, and maintaining web applications using the React.js library. You will collaborate with cross-functional teams to deliver high-quality, scalable, and performant solutions that meet our clients requirements.'
+        },
+        { id:104, name:'flutter developer', author: 'Steve rao',  type: 'stipend', 
+        salary: 145.50, rating:3.5,image:'assets/courses/70760bf1e88b184bb1bc.png',
+          description: 'Steve rao is a skilled Flutter Developer to join our mobile app development team. As a Flutter Developer, you will be responsible for designing, developing, and maintaining cross-platform mobile applications using the Flutter framework. You will collaborate with cross-functional teams to deliver high-quality, user-friendly, and performant mobile solutions that meet our clients requirements.'
+        },
+        { id:105, name:'docker developer', author: 'John Heikela', type: 'free', 
+        salary: 0.0, rating:4.7, image:'assets/courses/docker.png',
+          description: 'John Heikela is a skilled Docker Developer to join our DevOps team. As a Docker Developer, you will be responsible for designing, implementing, and maintaining containerized solutions using Docker technology. You will collaborate with cross-functional teams to deploy and manage applications in a scalable and efficient manner.'
+        },
+        { id:106, name:'Angular with .NET Core developers', author: 'Mark Vought',  type: 'stipend', 
+        salary: 129.00,rating:3.0, image:'assets/courses/course-image-6.jpeg',
+          description: 'Mark Vought is a talented Angular with .NET Core Developer to join our development team. As an Angular with .NET Core Developer, you will be responsible for designing, implementing, and maintaining web applications using Angular on the front-end and .NET Core on the back-end. You will collaborate with cross-functional teams to deliver high-quality, scalable, and performant solutions that meet our clients requirements.'
+        },
+        { id:107, name:'React native developer', author: 'Merry Smith',  type: 'Free', 
+        salary: 0.00, rating:3.75, image:'assets/courses/course-image-7.jpeg',
+          description: 'Merry Smith is a skilled React Native Developer to join our mobile development team. As a React Native Developer, you will be responsible for designing, developing, and maintaining cross-platform mobile applications using React Native framework. You will collaborate with cross-functional teams to deliver high-quality, user-friendly, and performant mobile solutions that meet our clients requirements.'
+        },
+        { id:108, name:'UX Design developers', author: 'Steve Silk', type: 'stipend', 
+        salary: 115.50, rating:5.0, image:'assets/courses/course-image-8.jpeg',
+          description: 'Steve Silk is a talented and creative UX Design Developer to join our team. As a UX Design Developer, you will be responsible for designing and implementing user-centered digital experiences that align with our business goals and user needs. You will collaborate with cross-functional teams to create intuitive and visually appealing user interfaces and ensure a seamless user experience.'
+        },
+        { id:109, name:'typescript developers', author: 'ram Smith',type: 'stipend', 
+        salary: 115.50, rating:2.0,image:'assets/courses/ts.png',
+          description: 'ram Smith is a skilled TypeScript Developer to join our development team. As a TypeScript Developer, you will be responsible for designing, developing, and maintaining robust and scalable web applications using TypeScript. You will collaborate with cross-functional teams to deliver high-quality, efficient, and maintainable code that meets our clients requirements.'
+        }
+      ]
+}
